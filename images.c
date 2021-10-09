4686f6cce72f42512f7ae77bde4b99c64a716e3f
ImperialOfficer324 <82236536+ImperialOfficer324@users.noreply.github.com>
2021-10-09 15:36:39 -0400

    Update images.c

--- images.c
+++ images.c
@@ -1,10 +1,15 @@
-/* images.c - load, display, then free images */
+/* images.c - main C file for EasyImages library. */
+/*
+    add this file to your Makefile.
+    include the images.h header file into the project file you want to draw images from.
+    add neccessary information to the load.c file and add that to your Makefile as well.
+    call LoadImages at the beginning of your program, after the SDL initiation.
+*/
 #include <SDL.h>
 #include <SDL2/SDL_image.h>
-#include "game.h"
 #include "images.h"
 
-// load images that are needed for the entire cycle. Other images can be loaded as needed
+// loads the renderer and loads images from load.c
 int LoadImages(){
     //renderer
     renderer = SDL_CreateRenderer(screen,-1,0);
@@ -15,11 +20,11 @@ int LoadImages(){
     return 1;
 }
 
-int FreeImages(){
-    return 1;
-}
+int FreeImages(
 
 /* Display Images */
+    
+// displays a full PNG image loaded directly from a file
 int RenderFromFile(char* filename,int x,int y,int w,int h){
     SDL_Surface* surface;
     SDL_Texture* texture;
@@ -45,6 +50,7 @@ int RenderFromFile(char* filename,int x,int y,int w,int h){
     return 1;
 }
 
+// displays a full PNG image from a surface
 int RenderFromSurface(SDL_Surface* surface,int x,int y,int w,int h){
     SDL_Texture* texture;
     SDL_Rect rect;
@@ -63,6 +69,7 @@ int RenderFromSurface(SDL_Surface* surface,int x,int y,int w,int h){
     return 1;
 }
 
+//draws the contents of the renderer to the screen. call this every frame
 int BlitDisplay(){
     SDL_RenderPresent(renderer);
     SDL_RenderClear(renderer);
