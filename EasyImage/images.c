275c16b805b94f2c4fa30920f4ba917c354b600e
ImperialOfficer324 <82236536+ImperialOfficer324@users.noreply.github.com>
2021-10-09 16:07:50 -0400

    Add EasyImage Folder

---
+++ EasyImage/images.c
@@ -0,0 +1,93 @@
+/* images.c - load, display, then free images */
+#include <SDL.h>
+#include <SDL2/SDL_image.h>
+#include "game.h"
+#include "images.h"
+
+// load images that are needed for the entire cycle. Other images can be loaded as needed
+int LoadImages(){
+    //renderer
+    renderer = SDL_CreateRenderer(screen,-1,0);
+    if (renderer == NULL){
+        printf("Error Loading Renderer: %s\n",SDL_GetError());
+        return 0;
+    };
+    return 1;
+}
+
+int FreeImages(){
+    return 1;
+}
+
+/* Display Images */
+int RenderFromFile(char* filename,int x,int y,int w,int h){
+    SDL_Surface* surface;
+    SDL_Texture* texture;
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = h;
+    surface = IMG_Load(filename);
+    if (surface == NULL){
+        printf("Error Loading Image: %s\n",SDL_GetError());
+        return 0;
+    };
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == NULL){
+        printf("Error Loading Texture: %s\n",SDL_GetError());
+        return 0;
+    };
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
+    
+    return 1;
+}
+
+int RenderFromSurface(SDL_Surface* surface,int x,int y,int w,int h){
+    SDL_Texture* texture;
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = h;
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == NULL){
+        printf("Error Loading Texture: %s\n",SDL_GetError());
+        return 0;
+    };
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_DestroyTexture(texture);
+    
+    return 1;
+}
+
+int RenderPartialImageFromSurface(SDL_Surface* surface,int x,int y,int w,int h,int px,int py,int pw,int ph){
+    SDL_Texture* texture;
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = w;
+    rect.h = h;
+    SDL_Rect port;
+    port.x = px;
+    port.y = py;
+    port.w = pw;
+    port.h = ph;
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == NULL){
+        printf("Error Loading Texture: %s\n",SDL_GetError());
+        return 0;
+    };
+    SDL_RenderCopy(renderer, texture, &port, &rect);
+    SDL_DestroyTexture(texture);
+    
+    return 1;
+}
+
+int BlitDisplay(){
+    SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+    return 1;
+}
